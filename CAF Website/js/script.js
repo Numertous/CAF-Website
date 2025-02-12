f43b78ed53c690ffe6e52e523d273176d0597150
Numertous <124647968+Numertous@users.noreply.github.com>
2025-02-12 09:13:30 -0600

    Add files via upload

---
+++ CAF Website/js/script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const menuButton = document.querySelector(".menu-button");
+    const dropdownMenu = document.querySelector(".dropdown-menu");
+
+    menuButton.addEventListener("click", function() {
+        dropdownMenu.classList.toggle("show");
+    });
+
+    document.addEventListener("click", function(event) {
+        if (!menuButton.contains(event.target) && !dropdownMenu.contains(event.target)) {
+            dropdownMenu.classList.remove("show");
+        }
+    });
+});
